5eaa1e2c9db9e909c0fcc955a720e0c3506ac756
youssef <youssefmounkaila@gmail.com>
2020-04-03 14:34:49 +0200

    add docker

--- README.md
+++
@@ -1,1 +0,0 @@
-# Project_v2
--- docker_v2/network.sh
+++ docker_v2/network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker network create -d macvlan --subnet=10.10.0.0/16 --gateway=10.10.0.1 -o macvlan_mode=bridge -o parent=eno1 macvlan_net
+sudo docker network create -d macvlan --subnet=10.10.0.0/16 --gateway=10.10.0.1 -o macvlan_mode=bridge -o parent=ens35u1c4i2 macvlan_net
 
